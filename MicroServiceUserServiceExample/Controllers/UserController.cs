02a0225efbff81a409aa02410fd42dd2f5491ac4
trautwein101 <trautwein101@yahoo.com>
2021-06-18 12:34:31 -0600

    setup controller
    This is controller to work with Ocelot, simple user table with entity for localhost....

---
+++ MicroServiceUserServiceExample/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroServiceUserServiceExample.Database;
+using MicroServiceUserServiceExample.Database.Entities;
+using Microsoft.AspNetCore.Http;
+
+namespace MicroServiceUserServiceExample.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        DatabaseContext db;
+
+        public UserController()
+        {
+            db = new DatabaseContext();
+        }
+
+        // GET: api/<userController>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return db.Users.ToList();
+        }
+
+        // GET: api/<UserController>/5
+        [HttpGet("{id}", Name = "Get")]
+        public User Get(int id)
+        {
+            return db.Users.Find(id);
+        }
+
+        //POST: api/<UserController>
+        [HttpPost]
+        public IActionResult Post([FromBody] User model)
+        {
+            try
+            {
+                db.Users.Add(model);
+                db.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, model);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+        }
+
+    }
+}
---
+++ MicroServiceUserServiceExample/Database/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using MicroServiceUserServiceExample.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroServiceUserServiceExample.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } //for working with entities
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(@"Server=localhost\SQLEXPRESS;Database=UserMicroservice;Trusted_Connection=True;MultipleActiveResultSets=True");
+        }
+
+
+    }
+}
